9f0a3ed68325d8e3b12030f36247a32393a51583
Kevin Rosario <29806319+KevinRosario01@users.noreply.github.com>
2024-07-13 00:39:24 +0000

    Second Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-full h-screen bg-green-100">
+          <div>RootLayout</div>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
